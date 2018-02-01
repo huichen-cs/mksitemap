cb460ea378be374c4152bd0433135406d4c9cab4
huichen-cs <chen@sci.brooklyn.cuny.edu>
2018-02-01 09:32:41 -0500

    updated

--- mksitemap.sh
+++ mksitemap.sh
@@ -1,25 +1,40 @@
 #!/bin/bash 
 
-fsitemap=sitemap.xml
 flist=`mktemp`;
-urlbase="http://huichen-cs.github.io"
 
 find content course -type f  \
     -iregex "^.*\.html\|^.*\.m\|^.*\.pdf\|^.*\.c\|^.*\.cpp\|^.*\.java" | \
     grep -v template >> ${flist}
 
-[ -r ${fsitemap} ] && rm -f ${fsitemap}
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ${fsitemap}
-echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> ${fsitemap}
+echo index.html >> ${flist}
 
-while read fn
-do
-    url=${urlbase}/${fn}
-    timestamp=`date -r "${fn}" -u +%Y-%m-%dT%H:%M:%S%:z`
-    echo "<url><loc>${url}</loc><lastmod>${timestamp}</lastmod></url>" >> ${fsitemap}
-done < ${flist}
+make_site_map() {
+    httpproto=$1
+    fsitemap=$2
 
-echo "</urlset>" >> ${fsitemap}
+    urlbase="${httpproto}://huichen-cs.github.io"
+
+    [ -r ${fsitemap} ] && rm -f ${fsitemap}
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > ${fsitemap}
+    echo "<urlset xmlns=\"${httpproto}://www.sitemaps.org/schemas/sitemap/0.9\">" >> ${fsitemap}
+
+    while read fn
+    do
+        if [[ ${fn} = *"leemis"* ]]; then
+            echo ${fn} is being excluded
+        else
+            url=${urlbase}/${fn}
+            timestamp=`date -r "${fn}" -u +%Y-%m-%dT%H:%M:%S%:z`
+            echo "<url><loc>${url}</loc><lastmod>${timestamp}</lastmod></url>" >> ${fsitemap}
+        fi
+    done < ${flist}
+
+    echo "</urlset>" >> ${fsitemap}
+}
+
+make_site_map https sitemap.xml
+make_site_map http ositemap.xml
+
 rm -f ${flist}
 
 
